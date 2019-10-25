bbc6b19c56c2583fc9b4f4a19ec7702582903180
Stephen Malito <stevemalito@gmail.com>
2019-10-25 11:48:29 -0500

    balanced

--- balancedStrings.js
+++ balancedStrings.js
@@ -8,10 +8,14 @@ Return the maximum amount of splitted balanced strings.
 */
 
 var balancedString = (s) => {
-
+   
+    // split the string into an array
+    let arry = s.split("")
+    console.log(arry)
+    if (arry[0] === arry [1])
 
 }
 
 balancedString("RLRRLLRLRL")
-balancedString("RLLLLRRRLR")
-balancedString("LLLLRRRR")
+// balancedString("RLLLLRRRLR")
+// balancedString("LLLLRRRR")
