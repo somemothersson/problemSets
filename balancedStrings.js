dea531966ed3ed34f70ce5d7c8f8edddb74c196e
Stephen Malito <stevemalito@gmail.com>
2019-10-25 11:28:37 -0500

    balString added

---
+++ balancedStrings.js
@@ -0,0 +1,17 @@
+/*
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s split it in the maximum amount of balanced strings.
+
+Return the maximum amount of splitted balanced strings.
+
+*/
+
+var balancedString = (s) => {
+
+
+}
+
+balancedString("RLRRLLRLRL")
+balancedString("RLLLLRRRLR")
+balancedString("LLLLRRRR")
